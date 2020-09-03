3008944eae1fa0bec5e12907e937c3dce8906335
Ariel Leon <arielleon@MacBook-Pro.local>
2020-09-02 23:31:17 -0400

    JudgeVegetables Solved

---
+++ judgeVegetable.js
@@ -0,0 +1,43 @@
+const judgeVegetable = function (vegetables, metric) {
+  var mostRedness = [];
+  for (i = 0; i < vegetables.length; i++ ) {
+    console.log(vegetables[i].redness)
+    mostRedness.push(vegetables[i].redness)
+  } 
+  
+  console.log(mostRedness);
+  var topScore = Math.max(...mostRedness);
+  console.log(topScore);
+  if (topScore === vegetables[0].redness) {
+    console.log(vegetables[0].submitter)
+    return vegetables[0].submitter
+  } else if (topScore === vegetables[1].redness) {
+    console.log(vegetables[1].submitter)
+    return vegetables[1].submitter
+  } else if (topScore === vegetables[2].redness) {
+    console.log(vegetables[2].submitter)
+    return vegetables[2].submitter
+  } 
+} 
+
+const vegetables = [
+  {
+    submitter: 'Old Man Franklin',
+    redness: 10,
+    plumpness: 5
+  },
+  {
+    submitter: 'Sally Tomato-Grower',
+    redness: 2,
+    plumpness: 8
+  },
+  {
+    submitter: 'Hamid Hamidson',
+    redness: 4,
+    plumpness: 3
+  }
+]
+
+const metric = vegetables.redness
+
+judgeVegetable(vegetables, metric)
--- smartGarbage.js
+++ smartGarbage.js
@@ -11,3 +11,8 @@ const smartGarbage = function(trash, bins) {
 }
 
 console.log(smartGarbage('recycling', { waste: 4, recycling: 2, compost: 5 }));
+
+/*first argument "trash" is passed on as a string through the console.log. 
+Second argument is an object that does not need to be delcared outside of the console.log.
+every time the string assigned to the trash argument is === to one of the values
+in the object bins. The number value increases by one */
